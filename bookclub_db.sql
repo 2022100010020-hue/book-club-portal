f97ad9decbf6a5aa7d1bb86675fc8ee410d9e8c3
2022100010020-hue <2022100010020@seu.edu.bd>
2026-06-15 00:05:24 +0600

    Update bookclub_db.sql

--- bookclub_db.sql
+++ bookclub_db.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 14, 2026 at 06:34 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `bookclub_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `announcements`
---
 
 CREATE TABLE `announcements` (
   `id` int(11) NOT NULL,
@@ -214,7 +189,3 @@ ALTER TABLE `reading_list`
   ADD CONSTRAINT `reading_list_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `reading_list_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
